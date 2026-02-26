0f631fe8db068ff5216682d3e59045c147f5a9dd
Raditya Zandra Fadhillah <radityazandra4@gmail.com>
2026-02-26 23:34:13 +0700

    P2_Praktikum2

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,27 @@
 <?php
-
+namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use Symfony\Component\Routing\Route as RoutingRoute;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+Route::get('/hello', [WelcomeController::class,'hello']);
 
 Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+Route::get('/about', [AboutController::class,'about']);
 
-Route::get('/about', function () {
-    return 'NIM : 244107020144 <br> Nama : Raditya Zandra Fadhillah';
-});
+Route::get('/', [HomeController::class,'index']);
 
 Route::get('/user/{name}', function ($name) { 
     return 'Nama saya '.$name; 
@@ -30,10 +31,18 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId){
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
 }); 
 
-Route::get('/articles/{id}', function ($id) { 
-    return 'Halaman Artikel dengan ID '.$id; 
-});
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
 
 Route::get('/user/{name?}', function ($name='John') { 
     return 'Nama saya '.$name; 
 }); 
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([ 
+    'index', 'show' 
+]); 
+
+Route::resource('photos', PhotoController::class)->except([ 
+    'create', 'store', 'update', 'destroy' 
+]);
